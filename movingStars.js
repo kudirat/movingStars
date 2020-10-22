14dee3912c1fddc2aeaa20bead11823ccd104847
kudirat <47795311+kudirat@users.noreply.github.com>
2020-10-21 19:20:45 -0500

    Update movingStars.js

--- movingStars.js
+++ movingStars.js
@@ -2,17 +2,23 @@ function setup() {
     var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
     var slider1 = document.getElementById('slider1');
-    slider1.value = -25;
+    slider1.value = -50;
 
     function draw() {
 	canvas.width = canvas.width;
-	var tParam = slider1.value*0.01;
+	var param = slider1.value*0.01;
 	
-	function moveToTx(loc,Tx)
-	{var res=vec2.create(); vec2.transformMat3(res,loc,Tx); context.moveTo(res[0],res[1]);}
+	function moveToTx(loc,Tx) {
+      var res=vec2.create();
+      vec2.transformMat3(res,loc,Tx);
+      context.moveTo(res[0],res[1]);
+    }
 
-	function lineToTx(loc,Tx)
-	{var res=vec2.create(); vec2.transformMat3(res,loc,Tx); context.lineTo(res[0],res[1]);}
+	function lineToTx(loc,Tx) {
+      var res=vec2.create();
+      vec2.transformMat3(res,loc,Tx);
+      context.lineTo(res[0],res[1]);
+    }
 
 //function used to draw a star object      
 function drawStar(cx, cy, spikes, outerRadius, innerRadius, outlineClr, fillClr, Tx) {
@@ -55,21 +61,8 @@ function drawStar(cx, cy, spikes, outerRadius, innerRadius, outlineClr, fillClr,
 	    return [x,y];
 	}
     
-function planet(color, outlineClr){
-var X = canvas.width / 2;
-var Y = canvas.height / 2;
-var R = 40;
-context.beginPath();
-context.arc(X, Y, R, 0, 2 * Math.PI, false);
-context.lineWidth = 7;
-context.strokeStyle = outlineClr;
-context.stroke();
-context.fillStyle = color;
-context.fill();
-}
-  
 
-	function drawTrajectory(t_begin,t_end,intervals,C,Tx,color) {
+	function trajectory(t_begin,t_end,intervals,C,Tx,color) {
 	    context.strokeStyle=color;
 	    context.beginPath();
             moveToTx(C(t_begin),Tx);
@@ -81,20 +74,18 @@ context.fill();
 	}
 
 	
-	var Tblue_to_canvas = mat3.create();
-	mat3.fromTranslation(Tblue_to_canvas,[150,150]);
-	mat3.scale(Tblue_to_canvas,Tblue_to_canvas,[1,-1]);
+	var to_canvas = mat3.create();
+	mat3.fromTranslation(to_canvas,[150,150]);
+	mat3.scale(to_canvas,to_canvas,[1,-1]);
 	
-	
-	drawTrajectory(0.0,3.0,100,spiral,Tblue_to_canvas,"#0c164f");
-	var Tgreen_to_blue = mat3.create();
-	mat3.fromTranslation(Tgreen_to_blue,spiral(tParam));
-	var Tgreen_to_canvas = mat3.create();
-	mat3.multiply(Tgreen_to_canvas, Tblue_to_canvas, Tgreen_to_blue);
-	//drawObject("green",Tgreen_to_canvas);
-    drawStar(10, 0, 5, 30, 15, "orange", "yellow", Tgreen_to_canvas);
+	trajectory(0.0,3.0,100,spiral,to_canvas,"#0c164f");
+	var spiral_toCanvas = mat3.create();
+	mat3.fromTranslation(spiral_toCanvas,spiral(param));
+	var spiral_def = mat3.create();
+	mat3.multiply(spiral_def, to_canvas, spiral_toCanvas);
+    drawStar(10, 0, 5, 30, 15, "orange", "yellow", spiral_def);
     
-      
+    //outer stars
     var gldStr = mat3.create();
     drawStar(29, 220, 5, 0, 15, "#ffcf40", "#3c1c31", gldStr);
     drawStar(29, 20, 5, 0, 15, "#ffbf00", "#3c1c31", gldStr);
@@ -107,13 +98,7 @@ context.fill();
     drawStar(140, 130, 5, 0, 15, "yellow", "#3c1c31", gldStr);
     drawStar(110, 170, 5, 0, 15, "#ffdc73", "#3c1c31", gldStr);
     drawStar(160, 170, 5, 0, 15, "#ffcf40", "#3c1c31", gldStr);
-    
-   // planet(color, outlineClr)
-    
-    
-    
     }
-    
     
   
     slider1.addEventListener("input",draw);
